84cda7641776cdea9e7ab2cf85007772c73875b1
Tobin801 <tand6053@740s-pl-103.local>
2015-09-15 13:01:06 -0600

    added getters and setters

--- src/marshmallows/model/MarshmallowMonster.java
+++ src/marshmallows/model/MarshmallowMonster.java
@@ -23,10 +23,61 @@ public class MarshmallowMonster
 		this.monsterHair = monsterHair;
 		this.monsterLegs = monsterLegs;
 	}
-
+	
+	public String getMonsterSring()
+	{
+		return monsterName;
+	}
+	
+	public int getMonsterEyes()
+	{
+		return monsterEyes;
+	}
+	
+	public int monsterNoses()
+	{
+		return monsterNoses;
+	}
+	
+	public double monsterLegs()
+	{
+		return monsterLegs;
+	}
+	
+	public double monsterHair()
+	{
+		return monsterHair;
+	}
+	
+	public boolean monsterBellyButton()
+	{
+		return monsterBellyButton;
+	}
+	
+	public void setmonsterName(String monsterName)
+	{
+		this.monsterName = monsterName;
+	}
+	
+	public void setmonsterEyes(int monsterEyes)
+	{
+		this.monsterEyes = monsterEyes;
+	}
+	
+	public void setmonsterNoses(int monsterNoses)
+	{
+		this.monsterNoses = monsterNoses;
+	}
+	
+	public void setmonsterLegs(double monsterLegs)
+	{
+		this.monsterLegs = monsterLegs;
+	}
+	
+	public void setmonsterHair
 	public String toString()
 	{
-		String monsterInfo = "My monster has " + monsterLegs + " legs and it's name is " + monsterName + ". It has " + monsterEyes + " eyes, the answer of wether or not he has a belly button is " + monsterBellyButton + ", ";
+		String monsterInfo = "My monster has " + monsterLegs + " legs and it's name is " + monsterName + ". It has " + monsterEyes + " eyes, the answer of wether or not he has a belly button is " + monsterBellyButton + ",  ";
 		
 		return monsterInfo;
 	}
