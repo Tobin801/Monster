3c77519cc8ea8171f2133385b9e1db05cd210a99
Tobin801 <tand6053@740s-pl-103.local>
2015-09-15 13:05:18 -0600

    Fix of past one

--- src/marshmallows/model/MarshmallowMonster.java
+++ src/marshmallows/model/MarshmallowMonster.java
@@ -34,47 +34,52 @@ public class MarshmallowMonster
 		return monsterEyes;
 	}
 	
-	public int monsterNoses()
+	public int getMonsterNoses()
 	{
 		return monsterNoses;
 	}
 	
-	public double monsterLegs()
+	public double getMonsterLegs()
 	{
 		return monsterLegs;
 	}
 	
-	public double monsterHair()
+	public double getMonsterHair()
 	{
 		return monsterHair;
 	}
 	
-	public boolean monsterBellyButton()
+	public boolean getMonsterBellyButton()
 	{
 		return monsterBellyButton;
 	}
 	
-	public void setmonsterName(String monsterName)
+	public void setMonsterName(String monsterName)
 	{
 		this.monsterName = monsterName;
 	}
 	
-	public void setmonsterEyes(int monsterEyes)
+	public void setMonsterEyes(int monsterEyes)
 	{
 		this.monsterEyes = monsterEyes;
 	}
 	
-	public void setmonsterNoses(int monsterNoses)
+	public void setMonsterNoses(int monsterNoses)
 	{
 		this.monsterNoses = monsterNoses;
 	}
 	
-	public void setmonsterLegs(double monsterLegs)
+	public void setMonsterLegs(double monsterLegs)
 	{
 		this.monsterLegs = monsterLegs;
 	}
 	
-	public void setmonsterHair
+	public void setMonsterHair(double monsterHair)
+	{
+		this.monsterHair = monsterHair;
+	}
+	
+	public void setMonsterBellyButton(boolean monsterBellyButton)
 	public String toString()
 	{
 		String monsterInfo = "My monster has " + monsterLegs + " legs and it's name is " + monsterName + ". It has " + monsterEyes + " eyes, the answer of wether or not he has a belly button is " + monsterBellyButton + ",  ";
